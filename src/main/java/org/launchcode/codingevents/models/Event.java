25d4469d7e21f5d602c0a1a2dbeece74aae197ca
Daryn <dhaffer1991@gmail.com>
2021-06-07 12:17:54 -0500

    Chapter 14 reading code-a-long

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -22,15 +22,28 @@ public class Event {
     @Email(message="Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    public EventType type;
+
+    public Event(String name, String description, String contactEmail, EventType type) {
+        this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.type = type;
+    }
+    public Event() {
         this.id = nextId;
         nextId++;
+    };
+
+    public EventType getType() {
+        return type;
     }
 
-    public Event() {};
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
 
     public String getContactEmail() {
         return contactEmail;
