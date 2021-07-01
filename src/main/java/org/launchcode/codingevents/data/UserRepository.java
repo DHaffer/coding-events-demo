c77ce6470622775973212334ef14375b34bec083
Daryn <dhaffer1991@gmail.com>
2021-07-01 14:36:38 -0500

    .

---
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -0,0 +1,8 @@
+package org.launchcode.codingevents.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUserName(String username);
+}
